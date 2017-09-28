7caee455228b7d278e5e2e3a304a0f3034a8ad0c
richardmckenzie <richard.mckenzie@gmail.com>
2017-09-28 21:06:22 +1300

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+
+//====LIST DEPENDENCIES===//
+const express = require('express');
+const parseurl = require('parseurl');
+const bodyParser = require('body-parser').json();
+const path = require('path');
+const expressValidator = require('express-validator');
+const mongoose = require('mongoose');
+const Signature = require('./models/signature.js')
+const app = express();
+const url = 'mongodb://richard.mckenzie:Risco123@ds029745.mlab.com:29745/signatures';
+//=========================//
+
+//====ROOT DIRECTORY===//
+app.get('/', function(req, res) {
+  res.json('you did it');
+});
+//==========================//
+//====GET ALL SIGNATURES===//
+app.get('/api/signatures', function(req, res) {
+  Signature.find({}).then(eachOne => {
+    res.json(eachOne);
+    })
+  })
+//==========================//
+//====POST NEW SIGNATURE===//
+app.post('/api/signatures',bodyParser ,function(req, res) {
+  Signature.create({
+    guestSignature: req.body.SignatureOfGuest,
+    message: req.body.MessageOfGuest,
+  }).then(signature => {
+    res.json(signature)
+  });
+});
+//==========================//
+
+//====MONGOOSE CONNECT===//
+mongoose.connect(url, function (err, db) {
+ if (err) {
+   console.log('Unable to connect to the mongoDB server. Error:', err);
+ } else {
+   console.log('Connection established to', url);
+ }
+});
+//==========================//
+
+app.listen(process.env.PORT || 3000);
+console.log('starting applicaiton.  Good job!');
